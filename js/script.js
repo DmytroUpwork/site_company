741d0aeb90edadd7ea0e2859ccf63cec2e1897cc
Dmitrii <lomonovskij@gmail.com>
2020-07-07 14:17:26 +0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,25 +1,46 @@
 'use strict'
 
 // elements DOM =====================================================
-let navbar_menu_toggler = document.querySelector('.navbar-toggler'),
-  navbar_menu_collapse = document.querySelector('.navbar_collapse'),
-  navbar_menu_toggler_line = document.querySelectorAll('.navbar_toggler_line');
-// function call =================================================
-navbar_menu_toggler.onclick = menu_mobile_toggler;
+let $navbar_menu_toggler = document.querySelector('.navbar-toggler'),
+  $navbar_menu_collapse = document.querySelector('.navbar_collapse'),
+  $navbar_button_line = document.querySelectorAll('.navbar-toggler span');
+
+ // function call =================================================
+$navbar_menu_toggler.onclick = menu_mobile_toggler;
+
 //function show menu button mobile ==================================
 function menu_mobile_toggler(event) {
-  navbar_menu_collapse.classList.toggle('hidden_mobile_li');
-  navbar_menu_toggler_line[0].classList.toggle('navbar_toggler_line_1_active');
-  navbar_menu_toggler_line[3].classList.toggle('navbar_toggler_line_1_active');
-  navbar_menu_toggler_line[1].classList.toggle('navbar_toggler_line_2_active');
-  navbar_menu_toggler_line[2].classList.toggle('navbar_toggler_line_3_active');
-  let nav_li = document.querySelectorAll('.nav-item');
-  for (let i = 0; i < nav_li.length; ++i) {
+  $navbar_menu_collapse.classList.toggle('hidden_mobile_li');
+  $navbar_button_line[0].classList.toggle("navbar_line_active_1");
+  $navbar_button_line[3].classList.toggle("navbar_line_active_1");
+  $navbar_button_line[1].classList.toggle("navbar_line_active_2");
+  $navbar_button_line[2].classList.toggle("navbar_line_active_3");
+  let $nav_li = document.querySelectorAll('.nav-item');
+  for (let i = 0; i < $nav_li.length; ++i) {
     setTimeout(() => {
-      nav_li[i].classList.toggle('active_li_mobile_menu');
+      $nav_li[i].classList.toggle('active_li_mobile_menu');
     }, i * 50);
   }
 };
+
+// вывод соц-иконок= ================================================
+let $wrapper_social = document.querySelector('.wrapper'),
+    $social_link = document.querySelectorAll(".wrapper a"),
+    $social_button = document.querySelector(".social_media_button");
+
+$social_button.onclick = social_toggler;
+
+function social_toggler() {
+  $wrapper_social.classList.toggle('wrapper_active');
+  for (let i = 0; i < $social_link.length; ++i) {
+    setTimeout(() => {
+      $social_link[i].classList.toggle("social_link_active");
+    }, i * 30);
+  }
+}
+
+
+
 
 // посимвольный ввод текста ==========================================
 const $sections = document.querySelectorAll('section');
@@ -31,8 +52,14 @@ for (const $section of $sections) {
     const $visible = $section.querySelector('.pageText');
     showSectionLetters($source, $visible, 0);
   };
+  // $section = function () {
+  //   if (section.style.class = 'active') {
+  //     let $source = $section.querySelector('.pageTextSource');
+  //     let $visible = $section.querySelector('.pageText');
+  //     showSectionLetters($source, $visible, 0);
+  //   }
+  // };
 }
-
 const showSectionLetters = ($source, $visible, marker) => {
   if ($visible.innerHTML.length === $source.innerHTML.length) return;
   // Добавляем 1 символ из $source в $visible
@@ -43,14 +70,76 @@ const showSectionLetters = ($source, $visible, marker) => {
   }
 };
 
+
+
+
+// let target = $(".elem");
+// let targetPos = target.offset().top;
+// let winHeight = $(window).height();
+// let scrollToElem = targetPos - winHeight;
+// $(window).scroll(function () {
+//   let winScrollTop = $(this).scrollTop();
+//   if (winScrollTop > scrollToElem) {
+//     //сработает когда пользователь доскроллит к элементу с классом .elem
+//   }
+// });
+
+
+
+
+
+
 //slide page ======================================================
 new fullpage('main', {});
 
-//======================================================
 
-// const wrapper = document.querySelector("main");
 
-// let direction = "";
+
+// carousel project work ============================================
+// let $btn_prev = document.querySelector('#gallery .buttons .prev');
+// let $btn_next = document.querySelector('#gallery .buttons .next');
+// let $images = document.querySelectorAll('#gallery .photos img');
+
+// let i = 0;
+
+// $btn_prev.onclick = function () {
+//   $images[i].style.display = "none";
+//   i--;
+//   if (i < 0) {
+//     i = $images.length - 1;
+//   }
+//   $images[i].style.display = "block";
+// };
+ 
+// $btn_next.onclick = function () {
+//   $images[i].style.display = 'none';
+//   i++;
+//   if ( i >= $images.length ) {
+//     i = 0;
+//   }
+//   $images[i].style.display = "block";
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const wrapper = document.querySelector('main');
+
+// let direction = '';
 // // Флаг, отвечающий за состояние скролинга
 // let isScrolling = false;
 
@@ -68,19 +157,19 @@ new fullpage('main', {});
 //   // Определения направления скролла
 
 //   if (e.deltaY > 0) {
-//     direction = "down";
+//     direction = 'down';
 //   } else if (e.deltaY < 0) {
-//     direction = "up";
+//     direction = 'up';
 //   }
 
 //   // Прокрутка блока в зависимости от положения скролла и направления движения
 //   if (wrapper.scrollTop === 0) {
-//     direction === "down" ? scrollAnimate(10) : () => {};
+//     direction === 'down' ? scrollAnimate(10) : () => {};
 //   } else if (Math.floor(wrapper.scrollTop) === lineHeight) {
-//     direction === "up" ? scrollAnimate(-10) : () => {};
+//     direction === 'up' ? scrollAnimate(-10) : () => {};
 //   } else if (Math.floor(wrapper.scrollTop) < lineHeight) {
 //     e.preventDefault();
-//     direction === "up" ? scrollAnimate(-10) : scrollAnimate(10);
+//     direction === 'up' ? scrollAnimate(-10) : scrollAnimate(10);
 //   }
 // };
 
